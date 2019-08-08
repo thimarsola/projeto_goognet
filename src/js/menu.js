81a7e0040c358f71f9cd51e90fc950a3edb77b50
Thiago Dias de Jesus Marsola Corrêa <thiagom@micromar.intranet>
2019-08-08 15:48:11 -0300

    correção do menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,9 @@
 $('.menu').click(function () {
 	$('.menu-site').toggleClass('menu-ativo');
+	$('.conteudo-header').addClass('nav-off');
 });
 
 $('.menu-site').click(function(){
 	$('.menu-site').removeClass('menu-ativo');
+	$('.conteudo-header').removeClass('nav-off');
 });
